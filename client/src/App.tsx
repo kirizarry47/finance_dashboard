92e058a986f4ecb7d27054f72f07fa7b304317fb
kirizarry47 <68522693+kirizarry47@users.noreply.github.com>
2023-04-06 17:09:31 -0400

    folder structure & navbar created

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,9 @@
 import { useMemo } from "react"
 import {ThemeProvider, createTheme} from "@mui/material/styles"
 import { themeSettings } from "./theme"
-import { CssBaseline } from "@mui/material"
+import { Box, CssBaseline } from "@mui/material"
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import  Navbar from "@/scenes/navbar";
 
 
 function App() {
@@ -9,9 +11,22 @@ function App() {
 
   return (
     <div className="app">
+      <BrowserRouter>
       <ThemeProvider theme={theme}>
         <CssBaseline/>
+        <Box width="100%" height="100%" padding="1rem 2rem 4rem 2rem" >
+          <Navbar/>
+          <Routes>
+            <Route path="/" element={<div>dashboard page</div>}/>
+              <Route path="/prediction" element={<div>prediction page</div>}/>
+              
+            
+
+          </Routes>
+
+        </Box>
       </ThemeProvider>
+      </BrowserRouter>
       
     </div>
   )
