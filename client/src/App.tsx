7602c387e7406b7415c3538d369934c2f8123633
kirizarry47 <68522693+kirizarry47@users.noreply.github.com>
2023-04-06 13:20:20 -0400

    MUI Config and themes expanded

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,17 @@
+import { useMemo } from "react"
+import {ThemeProvider, createTheme} from "@mui/material/styles"
+import { themeSettings } from "./theme"
+import { CssBaseline } from "@mui/material"
 
 
 function App() {
-  
+  const theme = useMemo(() => createTheme(themeSettings), [])
 
   return (
     <div className="app">
+      <ThemeProvider theme={theme}>
+        <CssBaseline/>
+      </ThemeProvider>
       
     </div>
   )
