2cb428fd60211fe37769c7229453b7b162f66c47
kirizarry47 <68522693+kirizarry47@users.noreply.github.com>
2023-04-10 11:37:39 -0400

    created the responsive grid layout

--- client/src/scenes/navbar/index.tsx
+++ client/src/scenes/navbar/index.tsx
@@ -17,7 +17,7 @@ const Navbar = (props: Props) => {
             <FlexBetween gap="0.75rem">
                 <PixIcon sx={{fontSize: "28px"}}/>
                 <Typography variant="h4" fontSize="16px">
-                    Seer Finance
+                    Growth
                 </Typography>
             </FlexBetween>
              {/* RIGHT SIDE */}
